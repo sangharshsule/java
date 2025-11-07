633f576858f192d3dfc2804d76064da9060ba4c9
Sangharsh Gautam Sule <159242820+sangharshsule@users.noreply.github.com>
2025-11-07 20:59:50 +0530

    Add files via upload

---
+++ HomeworkFinal.java
@@ -0,0 +1,28 @@
+package com.tka.sangharsh;
+
+public class Homework {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Binary NO Of 12 = "+Integer.toBinaryString(12));
+		System.out.println("Binary NO Of 15 = "+Integer.toBinaryString(15));
+		
+		
+		System.out.println("Binary NO Of 11 = "+Integer.toBinaryString(11));
+		System.out.println("Binary NO Of 13 = "+Integer.toBinaryString(13));
+		System.out.println("Binary NO Of 12 = "+Integer.toBinaryString(12));
+		
+		
+		System.out.println("Binary NO Of 17 = "+Integer.toBinaryString(17));
+		System.out.println("Binary NO Of 7 = "+Integer.toBinaryString(7));
+		System.out.println("Binary NO Of 49 = "+Integer.toBinaryString(49));
+		
+		
+		
+		System.out.println("Coversion Of 12 & 15 ="+(12 & 15));
+		System.out.println("Coversion Of 11 | 13 & 12 ="+(11 | 13 & 12));
+		System.out.println("Coversion Of 17 & 7 & 49 ="+(17 & 7 & 49));
+
+	}
+
+}
