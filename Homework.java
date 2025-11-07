e67e5a06445b54a04d2fa739b145ff8b3ae2a458
Sangharsh Gautam Sule <159242820+sangharshsule@users.noreply.github.com>
2025-11-07 20:29:57 +0530

    Add Homework class with bitwise operations

---
+++ Homework.java
@@ -0,0 +1,12 @@
+package com.tka.sangharsh;
+
+public class Homework {
+	public static void main(String[] args) {
+
+		System.out.println(12 & 15);
+		System.out.println(11 | 13 & 12);
+		System.out.println(17 & 7 & 49);
+
+	}
+
+}
