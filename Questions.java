d003a7661ec914226c72ca67677a63c00f287a80
Sangharsh Gautam Sule <159242820+sangharshsule@users.noreply.github.com>
2025-11-11 16:33:22 +0530

    Add files via upload

---
+++ Questions.java
@@ -0,0 +1,159 @@
+package com.tka.practice;
+
+public class Questions {
+	public static void main(String[] args) {
+		
+		
+		int a = 10; a += 5; a -= 3; a *= 2; a /= 4; 
+		System.out.println(a);
+//		//6
+//		
+		
+//		int a = 5, b = 10, c = 15; 
+//		System.out.println((a <b) && (b <c));
+//		System.out.println((a > b) || (b< c)); 
+//		System.out.println(!(a < c));
+		//true
+		//true
+		//false
+		
+//		int x = 10, y = 20; 
+//		System.out.println(x++ + ++y);
+		//31
+		
+		
+//		int a = 5, b = 2; 
+//		System.out.println(a % b);
+//		//1
+//		
+//		
+//		int p=7, q=3; 
+//		System.out.println(p*q+p/ q);
+//		//23
+//		
+//		
+//		
+//		int n = 5; 
+//		System.out.println(++n + n++);
+//		//12
+//		
+//		int x = 5, y = 10; 
+//		System.out.println(x>y?x:y);
+//		//10
+		
+//		
+//		int a = 10, b = 5, c; 
+//		c = a++ + --b + a*b; 
+//		System.out.println(c);
+		//58
+		
+		
+//		int a = 4, b = 2; 
+//		System.out.println(a<<b);
+		//16
+		
+		
+//		int a = 8, b = 2; 
+//		System.out.println(a>> b);
+		//2
+		
+		
+		
+//		int a = 10; 
+//		System.out.println(~a);
+		// -11
+		
+		
+//		int a = 5, b = 7; 
+//		System.out.println(a & b);
+		//5
+		
+		
+//		int a = 5, b = 7; 
+//		System.out.println(a | b);
+		//7
+		
+		
+//		int a = 5, b = 7; 
+//		System.out.println(a ^b);
+		//2
+		
+		
+//		int a = 10; 
+//		System.out.println(a++ + ++a + a--);
+		//34
+		
+		
+//		int a = 1; a += a++ + ++a; 
+//		System.out.println(a);
+		//5
+		
+		
+//		int a = 5, b = 10; boolean result = (a < b) ? true : false; 
+//		System.out.println(result);
+		//true
+		
+		
+//		int a = 5, b = 10; 
+//		System.out.println(a == b);
+		//false
+		
+		
+//		int a = 3, b = 2; 
+//		System.out.println(a * b + a / b);
+		//7
+		
+		
+////		int a = 5; a = a++ + ++a; 
+////		System.out.println(a);
+//		// 12
+		
+		
+//		int a = 10, b = 5; 
+//		System.out.println(a > b && a != b);
+		//true
+		
+//		
+//		int x = 2, y = 3; x*= y + 1; 
+//		System.out.println(x);
+		//8
+//		
+//		
+//		int a = 5; a += a -= a *= a; 
+//		System.out.println(a);
+		// -15
+//		
+//		
+//		int a = 4, b = 3; 
+//		System.out.println(a>b ? a++ : ++b);
+		//4
+		
+		
+//		int a = 5; 
+//		System.out.println(a++ + a++ + ++a);
+		//4
+		
+//		
+//		int a = 2, b = 3; 
+//		System.out.println(a + b * a / b);
+		//4
+//		
+//		
+//		int a = 10, b = 20; 
+//		System.out.println(a < b && b > a);
+		//true
+//		
+//		
+//		int a = 6, b = 2; 
+//		System.out.println((a / b) * (a % b));
+		//0
+//		
+//		
+//		int a = 5, b = 10, c = 15; 
+//		System.out.println((a < b && b > c) ||(b < c));
+//		System.out.println();
+		//true
+	}
+	
+
+}
