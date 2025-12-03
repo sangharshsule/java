7f34f09dd918891868c88136074af94d166533d6
Sangharsh Gautam Sule <159242820+sangharshsule@users.noreply.github.com>
2025-12-03 16:12:01 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,102 +1,73 @@
 package com.main;
 
-import com.vehicle.Vehicle;
+import com.question.Practice;
 
 public class Main {
 	public static void main(String[] args) {
 		
-        Vehicle v1 = new Vehicle();
-        v1.brand = "Toyota";
-        v1.model = "Fortuner";
-        v1.type = "SUV";
-        v1.fuelType = "Diesel";
-        v1.year = 2022;
-        v1.price = 3500000;
-
-        Vehicle v2 = new Vehicle();
-        v2.brand = "Maruti Suzuki";
-        v2.model = "Swift";
-        v2.type = "Hatchback";
-        v2.fuelType = "Petrol";
-        v2.year = 2021;
-        v2.price = 700000;
-
-        Vehicle v3 = new Vehicle();
-        v3.brand = "Tata";
-        v3.model = "Nexon";
-        v3.type = "SUV";
-        v3.fuelType = "Electric";
-        v3.year = 2023;
-        v3.price = 1600000;
-
-        Vehicle v4 = new Vehicle();
-        v4.brand = "Hyundai";
-        v4.model = "Creta";
-        v4.type = "SUV";
-        v4.fuelType = "Petrol";
-        v4.year = 2020;
-        v4.price = 1500000;
-
-        Vehicle v5 = new Vehicle();
-        v5.brand = "Honda";
-        v5.model = "City";
-        v5.type = "Sedan";
-        v5.fuelType = "Petrol";
-        v5.year = 2019;
-        v5.price = 1200000;
-
-        Vehicle v6 = new Vehicle();
-        v6.brand = "Mahindra";
-        v6.model = "Thar";
-        v6.type = "Off-road SUV";
-        v6.fuelType = "Diesel";
-        v6.year = 2024;
-        v6.price = 1900000;
-
-        Vehicle v7 = new Vehicle();
-        v7.brand = "Kia";
-        v7.model = "Seltos";
-        v7.type = "SUV";
-        v7.fuelType = "Petrol";
-        v7.year = 2022;
-        v7.price = 1700000;
-
-        Vehicle v8 = new Vehicle();
-        v8.brand = "BMW";
-        v8.model = "X5";
-        v8.type = "Luxury SUV";
-        v8.fuelType = "Diesel";
-        v8.year = 2023;
-        v8.price = 9000000;
-
-        Vehicle v9 = new Vehicle();
-        v9.brand = "Audi";
-        v9.model = "A6";
-        v9.type = "Luxury Sedan";
-        v9.fuelType = "Petrol";
-        v9.year = 2021;
-        v9.price = 6500000;
-
-        Vehicle v10 = new Vehicle();
-        v10.brand = "Royal Enfield";
-        v10.model = "Classic 350";
-        v10.type = "Motorcycle";
-        v10.fuelType = "Petrol";
-        v10.year = 2020;
-        v10.price = 220000;
-        
-        
-        System.out.println("Vehicle 1: " + v1.brand + ", " + v1.model + ", " + v1.type + ", " + v1.fuelType + ", " + v1.year + ", " + v1.price);
-        System.out.println("Vehicle 2: " + v2.brand + ", " + v2.model + ", " + v2.type + ", " + v2.fuelType + ", " + v2.year + ", " + v2.price);
-        System.out.println("Vehicle 3: " + v3.brand + ", " + v3.model + ", " + v3.type + ", " + v3.fuelType + ", " + v3.year + ", " + v3.price);
-        System.out.println("Vehicle 4: " + v4.brand + ", " + v4.model + ", " + v4.type + ", " + v4.fuelType + ", " + v4.year + ", " + v4.price);
-        System.out.println("Vehicle 5: " + v5.brand + ", " + v5.model + ", " + v5.type + ", " + v5.fuelType + ", " + v5.year + ", " + v5.price);
-        System.out.println("Vehicle 6: " + v6.brand + ", " + v6.model + ", " + v6.type + ", " + v6.fuelType + ", " + v6.year + ", " + v6.price);
-        System.out.println("Vehicle 7: " + v7.brand + ", " + v7.model + ", " + v7.type + ", " + v7.fuelType + ", " + v7.year + ", " + v7.price);
-        System.out.println("Vehicle 8: " + v8.brand + ", " + v8.model + ", " + v8.type + ", " + v8.fuelType + ", " + v8.year + ", " + v8.price);
-        System.out.println("Vehicle 9: " + v9.brand + ", " + v9.model + ", " + v9.type + ", " + v9.fuelType + ", " + v9.year + ", " + v9.price);
-        System.out.println("Vehicle 10: " + v10.brand + ", " + v10.model + ", " + v10.type + ", " + v10.fuelType + ", " + v10.year + ", " + v10.price);
-
+		Practice name = new Practice();
+		name.myName();
+		
+		Practice add = new Practice();
+		add.sum(10, 15);
+		
+		Practice age = new Practice();
+		age.myAge();
+		
+		Practice square = new Practice();
+		square.mySquare(5);
+		
+		Practice num = new Practice();
+		num.myNum();
+		
+		Practice no = new Practice();
+		no.num(15);
+		
+//		Practice fact = new Practice();
+//		fact.factorial(5);
+//		
+//		Practice Pri = new Practice();
+//		Pri.prime(22);
+		
+		Practice Lar = new Practice();
+		Lar.findLargest(32, 323, 389);
+		
+		Practice cou = new Practice();
+		cou.count(877);
+		
+		Practice s = new Practice();
+		s.mySqaure(5);
+		
+		Practice c = new Practice();
+		c.cube(3);
+		
+		Practice su = new Practice();
+		su.mySum(176, 546);
+		
+		Practice g = new Practice();
+		g.great(10, 15, 5);
+		
+		Practice e = new Practice();
+		e.even(11);
+		
+		Practice f = new Practice();
+		f.fact(5);
+		
+		Practice d = new Practice();
+		d.reverseNumber(1234);
+		
+		Practice p = new Practice();
+		p.isPrime(23);
+		System.out.println(p.isPrime(7));  // true
+		
+		
+		Practice count = new Practice();
+		count.countDigits(12345);
+		
+		Practice i = new Practice();
+		i.myInfo("Sangharsh", "Gautam", "Sule", 22, 7378765262l, 596665740104l,"PLGPS6529N", "PUNE", "STUDENT", "CODING");
+		
+		
 	}
 
 }
---
+++ Practice.java
@@ -0,0 +1,282 @@
+package com.question;
+
+
+
+public class Practice {
+
+//    1. Write a method to print your name (no return, no parameters).
+
+	public void myName() {
+		System.out.println("My name is Sangharsh Sule");
+
+	}
+
+//    2. Write a method to print the sum of two numbers (no return, with parameters).
+
+	public void sum(int a, int b) {
+		System.out.println("Sum of " + a + " and " + b + " = " + (a + b));
+
+	}
+//    3. Write a method that returns your age (return type, no parameters).
+
+	public void myAge() {
+		int age = 21;
+		System.out.println("My age is " + age);
+	}
+//    4. Write a method that returns the square of a number (return type, with parameters).
+
+	public void mySquare(int num) {
+		System.out.println("The Square of " + num + " is " + (num * num));
+	}
+//    5. Write a method to print the first 10 natural numbers.
+
+	public void myNum() {
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+//    6. Write a method to check whether a number is even or odd.
+	public void num(int num) {
+		if (num % 2 == 0) {
+			System.out.println(num + " This is Even Number");
+		} else {
+			System.out.println(num + " This is Odd Number");
+		}
+
+	}
+
+//    7. Write a method to calculate the factorial of a given number.
+	// Method without return type
+	public void factorial(int n) {
+		int fact = 1;
+
+		for (int i = 1; i <= n; i++) {
+			fact = fact * i;
+		}
+
+		System.out.println("Factorial of " + n + " is: " + fact);
+	}
+
+//    8. Write a method to check if a number is prime or not.
+
+	public void prime(int n) {
+		int count = 0;
+
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				count++;
+			}
+		}
+
+		if (count == 2) {
+			System.out.println(n + " is Prime");
+		} else {
+			System.out.println(n + " is Not Prime");
+		}
+	}
+
+//    9. Write a method to return the largest of three numbers.
+
+	public void findLargest(int a, int b, int c) {
+		int largest;
+
+		if (a >= b && a >= c) {
+			largest = a;
+		} else if (b >= a && b >= c) {
+			largest = b;
+		} else {
+			largest = c;
+		}
+
+		System.out.println("Largest Number is: " + largest);
+	}
+
+//    10. Write a method to count the number of digits in a number.
+
+	public void count(int n) {
+		int count = 0;
+		int num = n;
+
+		if (n == 0) {
+			count = 1;
+		} else {
+			while (n != 0) {
+				n = n / 10;
+				count++;
+			}
+		}
+
+		System.out.println("Number of digits in " + num + " = " + count);
+	}
+
+//	with return type
+
+//	 Write a method that returns the square of a number.
+
+	public int mySqaure(int num) {
+		num = num * num;
+
+		System.out.println("Square = " + num);
+		return num;
+	}
+
+//	 Write a method that returns the cube of a number.
+
+	public int cube(int num) {
+		num = num * num * num;
+		System.out.println("cube = " + num);
+		return num;
+	}
+
+//	 Write a method that returns the sum of two numbers.
+
+	public int mySum(int a, int b) {
+		int add = a + b;
+		System.out.println("Addition of " + a + " and " + b + " " + "is " + add);
+		return add;
+	}
+
+//	 Write a method that returns the greatest of three numbers
+
+	public int great(int a, int b, int c) {
+		int greatest;
+
+		if (a >= b && b >= c) {
+			greatest = a;
+		} else if (a <= b && b >= c) {
+			greatest = b;
+		} else {
+			greatest = c;
+		}
+
+		System.out.println("The greatest number is " + greatest);
+		return greatest;
+	}
+
+//	 Write a method that returns true if a number is even, else false.
+
+	public int even(int num) {
+
+		if (num % 2 == 0) {
+			System.out.println("True");
+
+		} else {
+			System.out.println("False");
+		}
+		return num;
+	}
+
+//	 Write a method that returns the factorial of a number. 
+
+	public int fact(int n) {
+		int fact = 1;
+
+		for (int i = 1; i <= n; i++) {
+			fact = fact * i;
+		}
+
+		System.out.println(fact);
+
+		return fact;
+	}
+
+//	Write a method that returns the reverse of a number.
+	public int reverseNumber(int n) {
+		int rev = 0;
+
+		while (n != 0) {
+			int digit = n % 10; // get last digit
+			rev = rev * 10 + digit; // add digit to reverse
+			n = n / 10; // remove last digit
+		}
+		System.out.println(rev);
+
+		return rev;
+	}
+
+//	 Write a method that returns whether a number is prime or not.
+
+	public boolean isPrime(int n) {
+		if (n <= 1) {
+			return false;
+		}
+
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		
+
+		return true;
+	}
+
+//	 Write a method that returns the count of digits in a number.
+
+	public int countDigits(int n) {
+		int count = 0;
+
+		// handle negative numbers
+		n = Math.abs(n);
+
+		// if number is 0, it has 1 digit
+		if (n == 0) {
+			return 1;
+		}
+
+		while (n > 0) {
+			n = n / 10;
+			count++;
+		}
+		System.out.println(count);
+
+		return count;
+	}
+	
+	
+	String fName;
+	String mName;
+	String lName;
+	int age;
+	long moNo;
+	long aadharNo;
+	String panNo;
+	String city;
+	String prof;
+	String hobby;
+	
+	
+	public void myInfo(String fName , String mName , String lName , int age , long moNo , long aadharNo , String panNo , String city , String prof , String hobby) {
+		
+		this.fName = fName;
+		this.mName = mName;
+		this.lName = lName;
+		this.age = age;
+		this.moNo = moNo;
+		this.aadharNo = aadharNo;
+		this.panNo = panNo;
+		this.city = city;
+		this.prof = prof;
+		this.hobby = hobby;
+		
+		
+		System.err.println("MY INFORMATION");
+		System.out.println("first name is "+ fName);
+		System.out.println("middle name is "+ mName);
+		System.out.println("last name is "+ lName);
+		System.out.println(" age = "+ age);
+		System.out.println("Mobile number = "+ moNo);
+		System.out.println("Adhar number = "+ aadharNo);
+		System.out.println("Pan Number =  "+ panNo);
+		System.out.println("live in "+ city);
+		System.out.println("I am a "+ prof);
+		System.out.println("I love  "+ hobby);
+		
+		
+	}
+	
+
+}
